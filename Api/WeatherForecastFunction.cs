0188c2b44b90bde842b47ebfe6f68891b58a9407
Pritish Nayak <pritishperfect@gmail.com>
2021-06-08 10:01:54 +0530

    moved Api to netcore3.1 for azure static app compatibility

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,18 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using Hmp.App.Shared;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Hmp.App.Shared;
 
 namespace Hmp.App.Api
 {
     public static class WeatherForecastFunction
     {
-        private const string FunctionName = "WeatherForecast";
         private static string GetSummary(int temp)
         {
             var summary = "Mild";
@@ -33,46 +31,22 @@ namespace Hmp.App.Api
             return summary;
         }
 
-        [Function(FunctionName)]
-        public static async Task<HttpResponseData> RunAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequestData req,
-            FunctionContext executionContext)
+        [FunctionName("WeatherForecast")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            var logger = executionContext.GetLogger(FunctionName);
-            logger.LogDebug("{FunctionName} is invoked.", FunctionName);
-
             var randomNumber = new Random();
             var temp = 0;
 
-            var forecastData = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = temp = randomNumber.Next(-20, 55),
                 Summary = GetSummary(temp)
             }).ToArray();
 
-            var response = req.CreateResponse();
-            await response.WriteAsJsonAsync(forecastData);
-
-            return response;
+            return new OkObjectResult(result);
         }
     }
-
-    //public static class Function1
-    //{
-    //    [Function("Function1")]
-    //    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
-    //        FunctionContext executionContext)
-    //    {
-    //        var logger = executionContext.GetLogger("Function1");
-    //        logger.LogInformation("C# HTTP trigger function processed a request.");
-
-    //        var response = req.CreateResponse(HttpStatusCode.OK);
-    //        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-    //        response.WriteString("Welcome to Azure Functions!");
-
-    //        return response;
-    //    }
-    //}
 }
